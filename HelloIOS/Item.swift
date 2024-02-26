35d2bfcc76326f72817e1ef36dc48c9da90a926a
cjy3676 <cjy3676@naver.com>
2024-02-26 22:45:26 +0900

    Initial Commit

---
+++ HelloIOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HelloIOS
+//
+//  Created by Jaeyeong Choe on 2/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
